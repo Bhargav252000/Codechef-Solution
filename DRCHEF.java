0b19c47bed47bca866af404a01762d1d3aa9b909
Bhargav25200 <bhargavgohil25@gmail.com>
2020-07-11 13:58:21 +0530

    DRCHEF 1 subtask remain

--- DRCHEF.java
+++ DRCHEF.java
@@ -1,9 +1,10 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
+
 //import java.util.*;
 /**
  * DRCHEF
  */
+//Author: Bhargav Gohil
 
 public class DRCHEF {
 
@@ -15,29 +16,32 @@ public class DRCHEF {
             int minDays = 0;
             int N = scan.nextInt();
             int X = scan.nextInt();
-            int []arr = new int [N];
+            //int []arr = new int [N];
+            ArrayList <Integer> arr = new ArrayList<Integer>();
             for(int i=0; i<N; i++){
-                arr[i] = scan.nextInt();
+                //arr[i] = scan.nextInt();
+                int t = scan.nextInt();
+                arr.add(t);
             }
-            Arrays.sort(arr);
-
+            Collections.sort(arr);
             for(int i=0; i<N; i++){
-                if(X >= arr[i]){
+                if(X >= arr.get(i)){
                     minDays++;
-                    X = Math.max(X, 2 * arr[i]);
+                    X = Math.max(X, 2 * arr.get(i));
                     continue;
                 }
-                int temp = arr[i];
+                int temp = arr.get(i);
+
                 while(temp > 0){
                     minDays++;
-                    temp = Math.min(temp, 2 * (temp-X));
-                    X = 2 * X; 
-
+                    temp = Math.min(temp, 2 * (temp - X));
+                    X = X * 2;
                 }
-                X = 2 * arr[i];
+                X = 2 * arr.get(i);
             }
 
             System.out.println(minDays);
+            
         }
         scan.close();
     }
